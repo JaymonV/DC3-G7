6ca88ffaa82dd23e167fd9d51f68a893df931871
jaymonv <47352670+jaymonv@users.noreply.github.com>
2019-09-13 17:47:03 +0200

    Update visualization water_pump.py

--- visualization water_pump.py
+++ visualization water_pump.py
@@ -10,33 +10,69 @@ Required:
         waterschap-aa-en-maas_sewage_2019 
     - Make sure merge_files.py is already executed
 """
+###############################################################################
+############################### SETTINGS ######################################
+plot_type = 'Summarize per day of week' # Sequential / Summarize per week / 
+                        # Summarize per day of week
+data = ['RG8170', 'Niveaumeting']
+data[1]
+
+# Sequential
+resample_unit_1 = 'H' # H: Hour, M: Month. Type: int unit ; for example 3M
+
+# Summarize per day of week
+resample_unit_2 = 'H'
+
+###############################################################################
 import pandas as pd
 import os
 import seaborn as sns
+import matplotlib.pyplot as plt
+import calendar
+days = list(calendar.day_name)
+
 sns.set(rc={'figure.figsize':(18, 6)})
 
 print('Reading data...')
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
-data_path = current_dir + '/waterschap-aa-en-maas_sewage_2019/sewer_data/data_pump'
-data_path_RG8150 = data_path + '/RG8150/RG8150/RG8150_overall.csv'
-data_path_RG8170 = data_path + '/RG8170/RG8170/RG8170_overall.csv'
+data_path = current_dir + \
+            '/waterschap-aa-en-maas_sewage_2019/sewer_data/data_pump/' + \
+            data[0] + '/' + data[0] + '/' + data[0] + '_overall.csv'
 
-rg8150 = pd.read_csv(data_path_RG8150, sep=';', decimal=',')
+rg8150 = pd.read_csv(data_path, sep=';', decimal=',')
 rg8150['TimeStamp'] = pd.to_datetime(rg8150['TimeStamp'], 
       format = "%d-%m-%Y %H:%M:%S")
 
-rg8150_niv = rg8150.loc[rg8150['Tagname'].str.contains('Niveaumeting'),]
+rg8150 = rg8150.loc[rg8150['Tagname'].str.contains(data[1]),]
+rg8150.index = rg8150['TimeStamp']
+rg8150 = rg8150[['TimeStamp', 'Value']]
 
 print('Finished. \n')
-
+    
 print('Preprocessing data...')
 
-rg8150_niv.index = rg8150_niv['TimeStamp']
-rg8150_niv = rg8150_niv[['Value', 'TimeStamp']].resample('H').mean()
+if plot_type == 'Sequential':
+    
+    rg8150 = rg8150[['Value', 'TimeStamp']].resample(resample_unit_1).mean()
 
+    print('Finished. \n')
+    
+    rg8150.plot()
+    
+elif plot_type == 'Summarize per week':
+    
+     rg8150 = rg8150.groupby(rg8150['TimeStamp'].dt.weekday).mean()
+     rg8150.plot()
 
-print('Finished. \n')
-
-rg8150_niv.plot()
+elif plot_type == 'Summarize per day of week':
+    rg8150['weekday'] = rg8150["TimeStamp"].dt.dayofweek
+    
+    for i in range(7):
+        plt.figure()
+        df = rg8150[rg8150['weekday']==i]
+        df = df.groupby(df.index.hour)['Value'].mean()
+        df.plot(title='24 hours of ' + data[1] + ' ' + data[0] + ' ' + days[i])
+        plt.savefig('24 hours of ' + data[1] + ' ' + data[0] + ' ' + \
+                    days[i] + '.png')
