53969ede2c71509737cfe116a43f1589b40c0feb
jaymonv <47352670+jaymonv@users.noreply.github.com>
2019-09-19 15:54:50 +0200

    Added support for WWTP flow data, added debug info

--- csv_file_merger.py
+++ csv_file_merger.py
@@ -6,14 +6,24 @@ Created on Thu Apr 25 10:38:02 2019
 
 Script used for merging the csv files in data folders. Supported:
     - sewer_data
+    - wwtp flow
 
-Preparation: (1) unzip the data folders, (2) place this file in the folder:
-    'waterschap-aa-en-maas_sewage_2019\sewer_data\' 
+Preparation: (1) unzip the data folders, (2) place this file in the folder for:
+    sewer_data - 'waterschap-aa-en-maas_sewage_2019\sewer_data\' 
+    wwtp flow  - 'waterschap-aa-en-maas_sewage_2019_db_pumps\sewer_data_db\
+                  data_wwtp_flow'
+
+  (3) Define list of folders (folder names variable) for:
+    sewer_data - ['RG8150/RG8150', 'RG8170/RG8170', 'rg8170_99', 
+                'rg8170_N99', 'RG8180_L0', 'RG8180_Q0']
+    wwtp flow - ['RG1876_flow', 'RG1882_flow']
 
 Output: '..._overall.csv' data file placed in each data folder
 """
+
 folder_names = ['RG8150/RG8150', 'RG8170/RG8170', 'rg8170_99', 
-                'rg8170_N99', 'RG8180_L0', 'RG8180_Q0']
+                'rg8170_N99', 'RG8180_L0', 'RG8180_Q0', 
+                'RG1876_flow', 'RG1882_flow']
 
 import pandas as pd
 import os
@@ -24,13 +34,29 @@ for folder in folder_names:
     
     df = pd.DataFrame()
     
-    path = current_dir + '/' + folder
+    folder_path = current_dir + '/' + folder
     
-    for file in os.listdir(path):
-        if 'overall' not in file:
-            print('merging file: ' + file)
-            df_temp = pd.read_csv(path + '/' + file, 
-                                  error_bad_lines=False, sep=';')
-            df = pd.concat([df, df_temp])
+    script_test = 0
     
-    df.to_csv(path + '/' + folder + '_overall.csv', index=False, sep=';')
+    if os.path.exists(folder_path):
+        
+        script_test = 1
+        
+        for file in os.listdir(folder_path):
+            if 'overall' not in file:
+                print('merging file: ' + file)
+                df_temp = pd.read_csv(folder_path + '/' + file, 
+                                      error_bad_lines=False, sep=';')
+                df = pd.concat([df, df_temp])
+
+    try:
+        df.to_csv(folder_path + '/' + folder + '_overall.csv', index=False, sep=';')
+    
+    except:
+        pass
+
+if script_test != 1:
+    print('No folder with data found to extract. Check script file location.')
+    
+else:
+    print('Merge succesful.')
