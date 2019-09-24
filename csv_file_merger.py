60f710510b33a481c796d40ac5a3076eaf4e6831
Veldkamp <j.veldkamp@student.tue.nl>
2019-09-24 18:13:20 +0200

    added merge support for data_pump_flow, rain_timeseries

--- csv_file_merger.py
+++ csv_file_merger.py
@@ -9,21 +9,21 @@ Script used for merging the csv files in data folders. Supported:
     - wwtp flow
 
 Preparation: (1) unzip the data folders, (2) place this file in the folder for:
-    sewer_data - 'waterschap-aa-en-maas_sewage_2019\sewer_data\' 
-    wwtp flow  - 'waterschap-aa-en-maas_sewage_2019_db_pumps\sewer_data_db\
-                  data_wwtp_flow'
-
-  (3) Define list of folders (folder names variable) for:
-    sewer_data - ['RG8150/RG8150', 'RG8170/RG8170', 'rg8170_99', 
-                'rg8170_N99', 'RG8180_L0', 'RG8180_Q0']
-    wwtp flow - ['RG1876_flow', 'RG1882_flow']
+  sewer_data      - 'waterschap-aa-en-maas_sewage_2019\sewer_data\' 
+  wwtp flow       - 'waterschap-aa-en-maas_sewage_2019_db_pumps\sewer_data_db\
+                      data_wwtp_flow'
+  data_pump_flow  - 'waterschap-aa-en-maas_sewage_2019_db_pumps\sewer_data_db\
+                     data_pump_flow'
+  rain_timeseries - 'waterschap-aa-en-maas_sewage_2019\sewer_data'
 
 Output: '..._overall.csv' data file placed in each data folder
 """
 
 folder_names = ['RG8150/RG8150', 'RG8170/RG8170', 'rg8170_99', 
                 'rg8170_N99', 'RG8180_L0', 'RG8180_Q0', 
-                'RG1876_flow', 'RG1882_flow']
+                'RG1876_flow', 'RG1882_flow', '1210FIT201_99',
+                '1210FIT301_99', '1210FIT401_94', '1210FIT501_99',
+                'rain_timeseries']
 
 import pandas as pd
 import os
@@ -55,7 +55,7 @@ for folder in folder_names:
                   sep=';')
     
     except:
-        print('none')
+        pass
 
 if script_test != 1:
     print('No folder with data found to extract. Check script file location.')
