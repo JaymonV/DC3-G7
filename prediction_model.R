447976804e7e87ff98a9fe786fe009ee98ced73f
Veldkamp <j.veldkamp@student.tue.nl>
2019-10-15 14:08:04 +0200

    Update prediction_model.R

--- prediction_model.R
+++ prediction_model.R
@@ -4,19 +4,21 @@ if(!require(forecast))install.packages("forecast")
 library(forecast)
 if(!require(lmtest))install.packages("lmtest")
 library(lmtest)
-
+library(ggplot2)
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
 ###############################################################
 ############################# NEW MODEL #######################
 ###############################################################
 
+timeslot = 48
+
 df <- read.csv('flow_level_and_rain.csv')
 
 unique(df[,'pump_station'])
 
 # Select model
-df <- df[df[,'pump_station'] == 'helftheuvelweg',]
+df <- df[df['pump_station'] == 'helftheuvelweg' & df['measurement_type'] == 'flow',]
 
 df[is.na(df[,'rainfall_volume']),'rainfall_volume'] <- 0
 rainfall <- df[,'rainfall_volume']
@@ -26,6 +28,10 @@ df[,'datetime'] <- as.Date(df[,'datetime'])
 
 df_ts <- ts(df[,'mean_value'], 
             start=min(df[,'datetime']))
+df_ts_train <- window(df_ts, end=(31930-timeslot))
+df_ts_test <- window(df_ts, start=(31931-timeslot))
+rainfall_train <- c(0, rainfall[1:(14398-timeslot)])
+rainfall_test <- rainfall[(14399-timeslot):14398]
 
 lags <- rep(0, 171)
 lags[167:171] <- NA
@@ -33,8 +39,19 @@ lags[24] <- NA
 
 length(lags)
 
-model <- Arima(df_ts, order=c(169, 0, 0), xreg=rainfall, method='CSS', fixed=lags)
+length(df_ts_train)
+length(rainfall_train)
+
+model <- Arima(df_ts_train, order=c(169, 1, 0), xreg=rainfall_train, method='CSS', fixed=lags, include.drift=TRUE)
+
+df_forecast <- forecast(model, df_ts_test, xreg=rainfall_test)
 
 coeftest(model)
 
+rain_df <- as.data.frame(list(rain=rainfall_test, Time=(31931-timeslot):31930))
+
+acf(diff(df_ts,1))
 df_aacf <- pacf(df_ts, lag=169)
+autoplot(df_forecast) +
+  xlim(32000-168, 31930)
+plot(df_forecast)
